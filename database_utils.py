a3a5eee9d20cfa331b6063470902410f43a77bb0
Fabio Frazao <fsfrazao@gmail.com>
2017-08-10 10:14:02 -0230

    Creates functions to create a database and tables

--- database_utils.py
+++ database_utils.py
@@ -1,42 +1,50 @@
 from tables import *
 
-def create_HDF_database(self,database_name):
+def create_database(file_name, simulation):
+    h5_database=open_file(file_name,mode="a")
+
+    sim_node="/sim_{0}".format(simulation)
+    try:
+        h5_database.get_node(sim_node)
+    except NoSuchNodeError:
+        sim=h5_database.create_group("/","sim_{0}".format(simulation),"Simulation {0}".format(simulation))
+
+    return h5_database
+
+
+def create_tables(h5_database,simulation=1):
 
     class Individuals(IsDescription):
-        time_stamp=StringCol()
+        time_stamp=StringCol(24)
         ind_id=Int32Col()
         pos_x=Float32Col()
         pos_y=Float32Col()
         energy=Float32Col()
 
     class Seeds(IsDescription):
-        deposition_time=StringCol()
+        deposition_time=StringCol(24)
         tree_id=Int32Col()
         initial_x=Float32Col()
         initial_y=Float32Col()
         final_x=Float32Col()
         final_y=Float32Col()
-        dispersal_type=StringCol()
+        dispersal_type=StringCol(24)
 
 
-    class Population(IsDescription):
-        time_stamp=StringCol()
+    class Populations(IsDescription):
+        time_stamp=StringCol(24)
         PrimaryDisperser=Int32Col()
 
 
 
-    h5file=open_file(database_name,mode="w",title="Tree Model outputs")
+    dispersers_lvl=h5_database.create_group("/sim_{0}".format(simulation),"dispersers","Dispersers Outputs for Simulation {0}".format(simulation))
+    sys_lvl=h5_database.create_group("/sim_{0}/dispersers".format(simulation),"sys_lvl","System Level Observations for Simulation {0}".format(simulation))
 
-    sim=h5file.create_group("/","sim_1","Simulation 1")
-    sys_lvl=h5file.create_group("/sim_1","sys_lvl","System Level Observations for simulation 1")
+    Pop_table=h5_database.create_table(sys_lvl,"Populations",Populations,"Disperser Population Sizes")
+    Seeds_table=h5_database.create_table(sys_lvl,"Seeds",Seeds,"Seed Dispersal Data")
 
-    NEE_table=h5file.create_table(sys_lvl,"NEE",NEE,"Net Ecosystem Exchange")
-    Pop_table=h5file.create_table(sys_lvl,"Pop",Populations,"Population Sizes")
-    Stocks_table=h5file.create_table(sys_lvl,"Stocks",CarbonStocks,"Carbon Stocks")
-    #Emissions_table=h5file.create_table(sys_lvl,"Emissions",CarbonEmissions,"Emissions from soil stocks, living trees and dead wood")
+    ind_lvl=h5_database.create_group("/sim_{0}/dispersers".format(simulation),"ind_lvl","Individual Level Observations for Simulation {0}".format(simulation))
 
-    ind_lvl=h5file.create_group("/sim_1","ind_lvl","Individual Level Observations for Simulation 1")
+    Ind_table=h5_database.create_table(ind_lvl,"Ind",Individuals,"Individual Level Data")
 
-    Ind_table=h5file.create_table(ind_lvl,"Ind",Individuals,"Individual Level Data")
-
-    h5file.close()
+    h5_database.close()
